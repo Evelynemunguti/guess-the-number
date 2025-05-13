9e793d6c3c1a89b0154b691881a97cf41093211e
Evelyne Munguti <evelynekyamwai@gmail.com>
2025-05-13 23:48:45 +0300

    Added the if statement to compare btn the computer choice and the palyer choice, if the numbers are same, its a win,if player choice is less then they lose and if its more they win

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //Math.Floor removes decimal places 
 //+1 ensures we get 1 to 100 not to 99 coz in Js no starts at 0
 
-let compNumber = Math.Floor(Math.random()*100)+1;
+let compNumber = Math.floor(Math.random()*100)+1;
 
 //to grab the InputDeviceInfo,guess and message from the html file
 //the players guess
@@ -15,10 +15,16 @@ const resultMessage = document.getElementById("resultMessage");
 // To Add a click event to the button
 submitGuess.addEventListener("click", 
     function(){
-const guess = Number(guessInput.Value); //helps turn input into a number
+const guess = Number(guessInput.value); //helps turn input into a number
 
+if (guess === compNumber){
+    resultMessage.textContent ="correct! You guessed right";  //this resultMessage.textContent stands for what will be returned
+}
+else if(guess < compNumber){
+    resultMessage.textContent = "Ohh No! Too low,Try again";
+}
+else if (guess > compNumber){
+    resultMessage.textContent = "Too high!,Try again";
+}
 
-
-
-
-})
+});
