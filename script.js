335f55f32b9df2f2f877f59e47cd4012b5effe47
Evelyne Munguti <evelynekyamwai@gmail.com>
2025-05-13 23:14:16 +0300

    Added the DOM getElementById to fetch data from the html file

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 //+1 ensures we get 1 to 100 not to 99 coz in Js no starts at 0
 
 let compNumber = Math.Floor(Math.random()*100)+1;
+
+//to grab the InputDeviceInfo,guess and message from the html file
+//the players guess
+
+
+const guessInput = document.getElementById ("guessInput");
+const submitGuess = document.getElementById ("submitGuess");
+const resultMessage = document.getElementById("resultMessage");
