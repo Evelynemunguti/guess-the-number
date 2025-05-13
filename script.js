de7221f879602e3d99ae0358031c7f875e51787b
Evelyne Munguti <evelynekyamwai@gmail.com>
2025-05-13 23:06:11 +0300

    Added Math.Floor and Random so that the computer will choose one number itself for comparison

---
+++ script.js
@@ -0,0 +1,5 @@
+//For the comp to choose a no btn 1to 100
+//Math.Floor removes decimal places 
+//+1 ensures we get 1 to 100 not to 99 coz in Js no starts at 0
+
+let compNumber = Math.Floor(Math.random()*100)+1;
